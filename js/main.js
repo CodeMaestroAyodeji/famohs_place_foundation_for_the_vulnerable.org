16c92ab541c88d4493c908ec0e0783ddf5e4e729
CodeMaestroAyodeji <ajuwon2024@gmail.com>
2025-10-15 09:55:02 +0100

    Updating the repo with pages modifications

--- js/main.js
+++ js/main.js
@@ -107,6 +107,39 @@
         ]
     });
 
+    // Contact Form Submission
+    var form = document.getElementById("contact-form");
+    var status = document.getElementById("form-status");
+
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    if (form) {
+        form.addEventListener("submit", handleSubmit)
+    }
     
 })(jQuery);
 
